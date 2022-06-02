beb7e836bec2f3c6324d86cb788ddf5e1cd85a91
pritesh511 <94275226+pritesh511@users.noreply.github.com>
2022-06-02 20:44:37 +0530

    database artist data save

--- src/components/addartistmodal/addartistmodal.js
+++ src/components/addartistmodal/addartistmodal.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   ArtistForm,
   ArtInputBlock,
@@ -8,26 +9,86 @@ import {
 } from "./styles";
 
 const AddArtistModal = ({ onClose }) => {
+  const [artist, setArtist] = useState({
+    name: "",
+    dob: "",
+    bio: "",
+  });
+  const handleChange = async (e) => {
+    e.preventDefault();
+    const { name, dob, bio } = artist;
+    await fetch("/artist", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        dob,
+        bio,
+      }),
+    }).then(() => alert("You have been added to the system!"));
+  };
   return (
     <>
-      <ArtistForm>
+      <ArtistForm
+        onSubmit={(e) => {
+          handleChange(e);
+          setArtist({
+            name: "",
+            dob: "",
+            bio: "",
+          });
+        }}
+      >
         <ArtInputBlock>
           <Label>Artist Name</Label>
-          <Input type="text" placeholder="Artist Name" />
+          <Input
+            type="text"
+            placeholder="Artist Name"
+            value={artist?.name}
+            onChange={(e) => {
+              setArtist({
+                ...artist,
+                name: e.target.value,
+              });
+            }}
+          />
         </ArtInputBlock>
         <ArtInputBlock>
           <Label>Date of Birth</Label>
-          <Input type="date" placeholder="Add Song Name" />
+          <Input
+            type="date"
+            placeholder="Add Song Name"
+            value={artist?.dob}
+            onChange={(e) => {
+              setArtist({
+                ...artist,
+                dob: e.target.value,
+              });
+            }}
+          />
         </ArtInputBlock>
         <ArtInputBlock>
           <Label>Artist Name</Label>
-          <TextArea type="text" placeholder="Add Song Name" />
+          <TextArea
+            type="text"
+            placeholder="Add Song Name"
+            value={artist?.bio}
+            onChange={(e) => {
+              setArtist({
+                ...artist,
+                bio: e.target.value,
+              });
+            }}
+          />
         </ArtInputBlock>
         <AddBtn
-          onClick={() => {
-            onClose();
-          }}
-          type="button"
+          // onClick={() => {
+          //   onClose();
+          // }}
+          type="submit"
         >
           Add Artist
         </AddBtn>
--- src/pages/add-song/add-song.js
+++ src/pages/add-song/add-song.js
@@ -15,21 +15,81 @@ import { Modal } from "baseui/modal";
 import { Select, TYPE } from "baseui/select";
 import AddArtistModal from "../../components/addartistmodal/addartistmodal";
 
+export const songData = {
+  songName: "",
+  releaseDate: "",
+  songImage: "",
+};
+
 const AddSong = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [value, setValue] = React.useState([]);
+  const [value, setValue] = useState([]);
+  const [songData, setSongData] = useState({
+    songName: "",
+    releaseDate: new Date(),
+    songImage: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { songName, releaseDate, songImage } = songData;
+    await fetch("http://localhost:4000/song", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Headers": "*",
+        crossdomain: true,
+      },
+      body: JSON.stringify({
+        songName,
+        releaseDate,
+        songImage,
+      }),
+    }).then(() => alert("You have been added to the system!"));
+  };
+
   return (
     <>
       <AddSongContainer>
         <Heading>Add Artist</Heading>
-        <AddSongForm>
+        <AddSongForm
+          onSubmit={(e) => {
+            handleSubmit(e);
+            setSongData({
+              songName: "",
+              releaseDate: new Date(),
+              songImage: "",
+            });
+          }}
+        >
           <InputBlock>
             <Label>Song Name</Label>
-            <Input type="text" placeholder="Add Song Name" />
+            <Input
+              type="text"
+              placeholder="Add Song Name"
+              value={songData?.songName}
+              onChange={(e) => {
+                setSongData({
+                  ...songData,
+                  songName: e.target.value,
+                });
+              }}
+            />
           </InputBlock>
           <InputBlock>
             <Label>Released Date</Label>
-            <Input type="date" placeholder="Add Song Name" />
+            <Input
+              type="date"
+              placeholder="Add Song Name"
+              value={songData?.releaseDate}
+              onChange={(e) => {
+                setSongData({
+                  ...songData,
+                  releaseDate: e.target.value,
+                });
+              }}
+            />
           </InputBlock>
           <InputBlock>
             <Label>Select Image</Label>
@@ -37,6 +97,13 @@ const AddSong = () => {
               type="file"
               accept="image/png, image/jpg, image/svg"
               placeholder="Add Song Name"
+              value={songData?.songImage}
+              onChange={(e) => {
+                setSongData({
+                  ...songData,
+                  songImage: e.target.value,
+                });
+              }}
             />
           </InputBlock>
           <ArtistSelectBlock>
@@ -72,13 +139,7 @@ const AddSong = () => {
               </AddBtn>
             </SelectWrap>
           </ArtistSelectBlock>
-          <AddBtn
-            className="submit-btn"
-            type="button"
-            onClick={() => {
-              setIsOpen(true);
-            }}
-          >
+          <AddBtn className="submit-btn" type="submit">
             Save
           </AddBtn>
         </AddSongForm>
